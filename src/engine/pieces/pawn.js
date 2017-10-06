16de2bc54da8c95e91da4dbd7ba9419d087ca7aa
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    made the end pawns move forward

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player.js';
+import Square from '../square.js';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,29 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        console.log(currentSquare);
+
+        let moves = [];
+        if (this.player === Player.WHITE){
+            let newRow = currentSquare.row;
+            newRow ++;
+            let newCol = 0;
+            const newSquare = new Square(newRow,newCol);
+            
+            console.log(newSquare);
+            moves.push(newSquare);
+
+        } else {
+            let newRow = currentSquare.row;
+            newRow--;
+            let newCol = 7;
+            const newSquare = new Square(newRow,newCol);
+            
+            console.log(newSquare);
+            moves.push(newSquare);
+        }
+        
+        return moves;
     }
 }
